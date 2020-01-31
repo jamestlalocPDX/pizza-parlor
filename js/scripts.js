81da322320357a800eb1209a5409d5152eea3228
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-31 11:26:51 -0800

    add pizza constructor and submit function in JavaScript as well as replace check boxes for pizza sizes with a selects input form.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Back-End Logic
+function Pizza () {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+$(document).ready(function() {
+  $("#pizza").submit(function(event) {
+    event.preventDefault();
+  })
+})
