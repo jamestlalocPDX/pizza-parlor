465c98cab2caa64df93126d9a037e61845bf144d
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-31 14:36:21 -0800

    remove front-end logic to start over from the back-end by adding properties to the Pizza constructor concerning the cost of size and number of toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 //Back-End Logic
-function Pizza () {
+function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = toppings;
-  this.cost = 0
+  this.toppings = {};
+}
+Pizza.prototype.calculateCost = function() {
+
 }
 
+//Front-End Logic
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
-    var size = $("#size").val();
-    var toppings = $(".toppings").val();
-    console.log(size);
-    console.log(toppings)
+    var inputtedSize = $("#size").val();
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var toppings = [];
+      var inputtedToppings = $(this).val();
+      toppings.push(inputtedToppings);
+    });
+    var newPizza = new Pizza(inputtedSize, inputtedToppings)
+    console.log(newPizza);
   })
 })
