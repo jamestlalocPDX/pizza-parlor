86a79ccbffac51a733893a449e8db5163c5d5442
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-31 16:21:50 -0800

    add prototype for Toppings constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Back-End Logic
-function Pizza(size) {
+function Pizza(size, toppings) {
   this.size = size;
-  this.toppings = [];
+  this.toppings = toppings;
   this.sizeCost = 0;
   this.toppingsCost = 0;
   this.totalCost = 0;
@@ -17,6 +17,7 @@ Pizza.prototype.pricesForSize = function() {
   return this.sizeCost = 15;
 }
 }
+console.log(this.sizeCost);
 Pizza.prototype.pricesForToppings = function() {
   
 }
@@ -26,11 +27,29 @@ return pricesForSize() + pricesForToppings();
 }
 
 function Toppings() {
-  this.pepperoni = 1;
-  this.sausage = 2;
-  this.pineapple = 3;
-  this.mushrooms = 1;
-  this.spinach = 1;
-  this.redonions = 1;
-  this.bananapeppers = 2;
+  this.pepperoni = pepperoni;
+  this.sausage = sausage;
+  this.pineapple = pineapple;
+  this.mushrooms = mushrooms;
+  this.spinach = spinach;
+  this.redonions = redonions;
+  this.bananapeppers = bananapeppers;
+}
+
+Toppings.prototype.pricesForToppings = function() {
+  if (this.pepperoni === "pepperoni") {
+    return this.pepperoni = 1;
+  } else if (this.sausage === "sausage") {
+    return this.sausage = 2;
+  } else if (this.pineapple === "pineapple") {
+    return this.pineapple = 3;
+  } else if (this.mushrooms === "mushrooms") {
+    return this.mushrooms = 2;
+  } else if (this.spinach === "spinach") {
+    return this.spinach = 1;
+  } else if (this.redonions === "redonions") {
+    return this.redonions = 1;
+  } else if (this.bananapeppers === "bananapeppers") {
+    return this.bananapeppers = 2
+  }
 }
